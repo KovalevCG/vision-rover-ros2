18686febb059b608ecfb115cef3ab03c8d443487
Alexander Kovalev <kovalev.cg@gmail.com>
2024-09-07 16:30:13 -0400

    my commit

--- src/my_package/launch/talker.launch.py
+++ src/my_package/launch/talker.launch.py
@@ -1,9 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-# sdfsf
-# print ("Test")
-
 def generate_launch_description():
     return LaunchDescription([
         Node(
